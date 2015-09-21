5ad7ac95f0e25059d708dd7824bd50b311a969fc
Alex <amvvanderwal@live.nl>
2015-09-21 12:25:34 +0200

    Hoofdscherm begin aangemaakt, dennis! werk

--- src/HoofdScherm.java
+++ src/HoofdScherm.java
@@ -1,4 +1,15 @@
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-public class HoofdScherm {
+public class HoofdScherm extends StackPane implements BeheerstScherm{
 
+	public SchermenController schermenController;
+	private Button home;
+	private Button mailMenu;
+	@Override
+	public void setSchermManager(SchermenController schermController) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
