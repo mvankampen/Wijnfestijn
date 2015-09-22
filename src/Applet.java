c8ee5a7557bd9e193ee1f813167600c6e59a96e7
Michael <michael1vk@hotmail.com>
2015-09-22 12:20:46 +0200

    Registration has a View

--- src/Applet.java
+++ src/Applet.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -13,16 +12,18 @@ public class Applet extends Application {
     private static final String MAILID = "mail";
     private static final String CUSTOMERSID = "customers";
     private static final String DEBITEURENID = "debiteuren";
-    
+    private static final String REGISTRATIONID = "registration";
+
     public void start(Stage stage) {
-    	//maakt de controller voor de schermen aan, handelt het display van de schermen af.
+        //maakt de controller voor de schermen aan, handelt het display van de schermen af.
         fillScreensController();
         navigationController = new NavigationController(screensController);
         //zet wel scherm er actief moet zijn
         Group root = new Group();
-        root.getChildren().addAll(screensController,navigationController);
-        Scene scene = new Scene(root,1200,800);
-        scene.getStylesheets().addAll(this.getClass().getResource("style/style.css").toExternalForm());
+        root.getChildren().addAll(screensController, navigationController);
+        Scene scene = new Scene(root, 1200, 800);
+        scene.getStylesheets()
+            .addAll(this.getClass().getResource("style/style.css").toExternalForm());
         stage.setScene(scene);
         stage.setWidth(1200);
         stage.setHeight(800);
@@ -30,37 +31,45 @@ public class Applet extends Application {
         stage.setTitle("A WarnerBrothers Product");
         stage.show();
 
-    };
-    private void fillScreensController(){
-    	screensController = new ScreensController();
-    	screensController.screenLoad(Applet.getHomeid(), new HomeView());
+    }
+
+    private void fillScreensController() {
+        screensController = new ScreensController();
+        screensController.screenLoad(Applet.getHomeid(), new HomeView());
         screensController.screenLoad(Applet.getOrderlistprintid(), new OrderlistPrintView());
         screensController.screenLoad(Applet.getMailid(), new MailView());
         screensController.screenLoad(Applet.getCustomersid(), new CustomersView());
-        screensController.screenSet(Applet.getHomeid());
+        screensController.screenLoad(Applet.getRegistrationid(), new RegistrationView());
+        screensController.screenSet(Applet.getRegistrationid());
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 
-	public static String getHomeid() {
-		return HOMEID;
-	}
-	public static String getOrderlistprintid() {
-		return ORDERLISTPRINTID;
-	}
+    public static String getHomeid() {
+        return HOMEID;
+    }
 
-	public static String getMailid() {
-		return MAILID;
-	}
+    public static String getOrderlistprintid() {
+        return ORDERLISTPRINTID;
+    }
 
-	public static String getDebiteurenid() {
-		return DEBITEURENID;
-	}
+    public static String getMailid() {
+        return MAILID;
+    }
 
-	public static String getCustomersid() {
-		return CUSTOMERSID;
-	}
+    public static String getDebiteurenid() {
+        return DEBITEURENID;
+    }
+
+    public static String getCustomersid() {
+        return CUSTOMERSID;
+    }
+
+    public static String getRegistrationid() {
+        return REGISTRATIONID;
+    }
+
 
 }
--- src/NavigationController.java
+++ src/NavigationController.java
@@ -1,6 +1,5 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
@@ -8,63 +7,65 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
 public class NavigationController extends AnchorPane {
-	private Button homeButton, mailButton, customersButton, debtorsButton;
-	private ComboBox<String> orderMenu;
-	private ObservableList<String> orderOptions;
-	private ScreensController screensController;
-	public NavigationController(ScreensController screensController){
-		this.screensController = screensController;
-		generateDropDownOptions();
-		createMenu();
-	}
-	
-	public void createMenu(){
-		//Home Button
-		GridPane navGrid = new GridPane();
-		navGrid.setVgap(59);
-		navGrid.setHgap(10);
-		HBox hbButtons = new HBox();
-		homeButton = new Button("Home");
-		
-		homeButton.getStyleClass().add("nav_item");
-		homeButton.setOnAction(e -> {
-			screensController.screenSet(Applet.getHomeid());
-			
-		});
-		//Mail Button
-		mailButton = new Button("Mail Menu");
-		mailButton.getStyleClass().add("nav_item");
-		mailButton.setOnAction(e -> {
-			screensController.screenSet(Applet.getMailid());
-			
-		});
-		//Order ComboBox
-		orderMenu = new ComboBox<String>(orderOptions);
-		orderMenu.setValue("Bestel Menu");
-		orderMenu.getStyleClass().add("nav_item");
-		//Customer Button
-		customersButton = new Button("Klanten Menu");
-		customersButton.getStyleClass().add("nav_item");
-		customersButton.setOnAction(e -> {
-			screensController.screenSet(Applet.getCustomersid());
-			
-		});
-		//Debtors Button
-		debtorsButton = new Button("Debiteuren Menu");
-		debtorsButton.getStyleClass().add("nav_end");
-		
-		//make the NavBar
-		hbButtons.getChildren().addAll(homeButton,mailButton, orderMenu, customersButton,debtorsButton );
-		navGrid.add(hbButtons, 2, 2);
-		getChildren().addAll(navGrid);
-	}
-	
-	
-	private void generateDropDownOptions(){
-		orderOptions = FXCollections.observableArrayList("option 1","option 2");// not done
-	}
+    private Button homeButton, mailButton, debtorsButton;
+    private ComboBox<String> orderMenu;
+    private ComboBox<String> customerMenu;
+    private ObservableList<String> orderOptions;
+    private ObservableList<String> customerOptions;
+    private ScreensController screensController;
+
+    public NavigationController(ScreensController screensController) {
+        this.screensController = screensController;
+        generateDropDownOptions();
+        createMenu();
+    }
+
+    public void createMenu() {
+        //Home Button
+        GridPane navGrid = new GridPane();
+        navGrid.setVgap(59);
+        navGrid.setHgap(10);
+        HBox hbButtons = new HBox();
+        homeButton = new Button("Home");
+
+        homeButton.getStyleClass().add("nav_item");
+        homeButton.setOnAction(e -> {
+            screensController.screenSet(Applet.getHomeid());
+
+        });
+        //Mail Button
+        mailButton = new Button("Mail Menu");
+        mailButton.getStyleClass().add("nav_item");
+        mailButton.setOnAction(e -> {
+            screensController.screenSet(Applet.getMailid());
+
+        });
+        //Order ComboBox
+        orderMenu = new ComboBox<String>(orderOptions);
+        orderMenu.setValue("Bestel Menu");
+        orderMenu.getStyleClass().add("nav_item");
+        //Customer ComboBox
+        customerMenu = new ComboBox<String>(customerOptions);
+        customerMenu.setValue("Klanten Menu");
+        customerMenu.getStyleClass().add("nav_item");
+
+        //Debtors Button
+        debtorsButton = new Button("Debiteuren Menu");
+        debtorsButton.getStyleClass().add("nav_end");
+
+        //make the NavBar
+        hbButtons.getChildren()
+            .addAll(homeButton, mailButton, orderMenu, customerMenu, debtorsButton);
+        navGrid.add(hbButtons, 2, 2);
+        getChildren().addAll(navGrid);
+    }
+
+
+    private void generateDropDownOptions() {
+        orderOptions = FXCollections.observableArrayList("option 1", "option 2");// not done
+        customerOptions = FXCollections.observableArrayList("registratie", "bewerken");
+    }
 
 
 
-	
 }
