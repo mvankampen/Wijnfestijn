7b896955fd42a50889a15ed11b7b226a8975757e
Alex <amvvanderwal@live.nl>
2015-09-24 16:04:52 +0200

    Home&Mail view edited by Dennis, Style accordingly changes

--- src/views/HomeView.java
+++ src/views/HomeView.java
@@ -1,29 +1,108 @@
-package views;
 
-import controllers.ScreensController;
-import interfaces.ControlledScreen;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
-public class HomeView extends AnchorPane implements ControlledScreen {
+public class HomeView extends AnchorPane implements ControlledScreen{
 
-    private ScreensController screensController;
-    private Label TestLabel;
+	private ScreensController screensController;
 
-    public void setScreenController(ScreensController screensController) {
-        this.screensController = screensController;
-    }
+	public void setScreenController(ScreensController screensController) {
+		this.screensController = screensController;
+	}
 
-    public HomeView() {
-        createView();
-    }
-
-    private void createView() {
-        getStyleClass().add("background");
-        setMinSize(1200, 800);
-        TestLabel = new Label("This is test");
-        TestLabel.setLayoutY(200);
-        getChildren().add(TestLabel);
-    }
+	public HomeView() {
+		createView();
+	}
+	private void createView() {
+		getStyleClass().add("background");
+		setMinSize(1200,800);
+		setUpContentPane();
+	}
+	
+	public void setUpContentPane(){
+		//Create introduction HBox
+		HBox introBox = new HBox();
+		introBox.setLayoutY(200);
+		introBox.setLayoutX(100);
+		//Create options Pane
+		GridPane optionsPane = new GridPane();
+		optionsPane.setLayoutY(200);
+		
+		//intro Box
+		Label introLabel = new Label("Dit menu is voorzien van alle directe links naar alle functies."
+				+ " Klik op de actie die u wilt ondernemen");
+		introBox.getChildren().add(introLabel);
+		
+		//Label texts row 1
+		String orderTitle = "Bestellijst opties";
+		String orderText1 = "Printen persoonlijke bestellijsten";
+		String orderText2 = "Invoeren van de persoonlijke bestellijsten (maakt orders en facturen)";
+		String orderText3 = "Maak een nieuwe bestellijst aan(Doormiddel van CSV";
+		String orderText4 = "Bewerk of verwijder een al bestaande bestellijst";
+		String debtorsTitle = "Debiteuren opties";
+		String debtorText = "Genereer de debiteurenlijst";
+		
+		//Label texts row 2
+		String mailTitle = "Mail opties";
+		String mailText1 = "Maak en verstuur een uitnodigingsmail";
+		String mailText2 = "Maak en verstuur een bedankmail";
+		String mailText3 = "Maak en verstuur een herinneringsmail";
+		String customerTitle = "Klanten opties";
+		String customerText1 = "Bewerk of verwijder bestaande klanten";
+		String customerText2 = "Open het klanten registreren scherm";
+		
+		//Creating labels row 1
+		Label orderTitleLabel = new Label(orderTitle);
+		orderTitleLabel.getStyleClass().add("homeview_title");
+		Label orderLabel1 = new Label(orderText1);
+		Label orderLabel2 = new Label(orderText2);
+		Label orderLabel3 = new Label(orderText3);
+		Label orderLabel4 = new Label(orderText4);
+		Label debtorsTitleLabel = new Label(debtorsTitle);
+		debtorsTitleLabel.getStyleClass().add("homeview_title");
+		Label debtorsLabel = new Label(debtorText);
+		
+		//Creating label row 2
+		Label mailTitleLabel = new Label(mailTitle);
+		mailTitleLabel.getStyleClass().add("homeview_title");
+		Label mailLabel1 = new Label(mailText1);
+		Label mailLabel2 = new Label(mailText2);
+		Label mailLabel3 = new Label(mailText3);
+		Label customerTitleLabel = new Label(customerTitle);
+		customerTitleLabel.getStyleClass().add("homeview_title");
+		Label customerLabel1 = new Label(customerText1);
+		Label customerLabel2 = new Label(customerText2);
+		
+		//Adding labels row 1
+		optionsPane.add(new Label(), 1, 1);
+		optionsPane.add(orderTitleLabel, 1, 2);
+		optionsPane.add(orderLabel1, 1, 3);
+		optionsPane.add(orderLabel2, 1, 4);
+		optionsPane.add(orderLabel3, 1, 5);
+		optionsPane.add(orderLabel4, 1, 6);
+		optionsPane.add(debtorsTitleLabel, 1, 7);
+		optionsPane.add(debtorsLabel, 1, 8);
+		
+		//Adding label row 2
+		optionsPane.add(mailTitleLabel, 2, 2);
+		optionsPane.add(mailLabel1, 2, 3);
+		optionsPane.add(mailLabel2, 2, 4);
+		optionsPane.add(mailLabel3, 2, 5);
+		optionsPane.add(customerTitleLabel, 2, 6);
+		optionsPane.add(customerLabel1, 2, 7);
+		optionsPane.add(customerLabel2, 2, 8);
+		
+		optionsPane.setHgap(100);
+		optionsPane.setVgap(10);
+		
+		getChildren().addAll(introBox, optionsPane);
+	}
 }
 
--- src/views/MailView.java
+++ src/views/MailView.java
@@ -1,29 +1,93 @@
-package views;
-
-import controllers.ScreensController;
-import interfaces.ControlledScreen;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class MailView extends AnchorPane implements ControlledScreen {
+public class MailView extends AnchorPane implements ControlledScreen{
 
-    private ScreensController screensController;
-    private Label TestLabel;
+	private ScreensController screensController;
 
-    public void setScreenController(ScreensController screensController) {
-        this.screensController = screensController;
-    }
+public void setScreenController(ScreensController screensController) {
+		this.screensController = screensController;	
+	}
 
-    public MailView() {
-        createView();
-    }
-
-    private void createView() {
-        getStyleClass().add("background");
-        setMinSize(1200, 800);
-        TestLabel = new Label("This is t3st");
-        TestLabel.setLayoutY(200);
-        TestLabel.setLayoutX(20);
-        getChildren().add(TestLabel);
-    }
+	public MailView() {
+		createView();
+	}
+	
+	private void createView() {
+		getStyleClass().add("background");
+		setMinSize(1200,800);
+		setUpContentPane();
+	}
+	
+	public void setUpContentPane(){
+		//contentPane details
+		GridPane contentPane = new GridPane();
+		contentPane.setLayoutY(175);
+		contentPane.setLayoutX(100);
+		contentPane.setVgap(25);
+		contentPane.setHgap(150);
+		
+		//Intro label
+		Label introLabel = new Label("Hier kunt u de mails opstellen. Kies welke mail u wilt"
+				+ " opstellen en voer\nde titel en body in. Klik op verzenden en de applicatie "
+				+ " doet de rest");
+		
+		//Create VBoxes row 1
+		VBox titleBox = new VBox(15);
+		VBox bodyBox = new VBox(15);
+		
+		//Create VBoxes row 2
+		VBox typeBox = new VBox(10);
+		
+		//Label texts row 1
+		String titleText = "Vul hier de titel in:";
+		String bodyText = "Vul de body in";
+		
+		//Label texts row 2
+		String typeText = "Selecteer wat voor een soort\nmail u wilt verzenden";
+		String selectionText = "Met deze selectie wordt de\nmail verstuur naar:";
+		
+		//Creating items row 1
+		Label titleLabel = new Label(titleText);
+		Label bodyLabel = new Label(bodyText);
+		TextField titleTextArea = new TextField();
+		TextArea bodyTextArea = new TextArea();
+		Label selectionLabel = new Label(selectionText);
+		
+		//Creating items row 2
+		Label typeLabel = new Label(typeText);
+		final ToggleGroup inviteGroup = new ToggleGroup();
+		RadioButton remindMailRb = new RadioButton("Herrineringsmail");
+		remindMailRb.setToggleGroup(inviteGroup);
+		RadioButton inviteMailRb = new RadioButton("Uitnodigingsmail");
+		inviteMailRb.setToggleGroup(inviteGroup);
+		RadioButton thankMailRb = new RadioButton("Bedankmail");
+		thankMailRb.setToggleGroup(inviteGroup);
+		Button printButton = new Button("Printen");
+		printButton.getStyleClass().add("form_buttons");
+		
+		//Adding to VBoxes
+		titleBox.getChildren().addAll(titleLabel, titleTextArea);
+		bodyBox.getChildren().addAll(bodyLabel, bodyTextArea);
+		typeBox.getChildren().addAll(remindMailRb, inviteMailRb, thankMailRb);
+		
+		//Adding items row 1
+		contentPane.add(introLabel, 0, 1);
+		contentPane.add(titleBox, 0, 2);
+		contentPane.add(bodyBox, 0, 3);
+		contentPane.add(typeLabel, 1, 1);
+		contentPane.add(typeBox, 1, 2);
+		contentPane.add(selectionLabel, 1, 3);
+		contentPane.add(printButton, 1, 4);
+		
+		//Add the contentPane
+		getChildren().add(contentPane);
+	}
 }
