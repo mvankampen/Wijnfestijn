db352303e4cd456075f2b5b01dea3b967ecca8a8
Michael <michael1vk@hotmail.com>
2015-09-28 14:34:55 +0200

    Database connection is working

---
+++ src/DAO/Database.java
@@ -0,0 +1,45 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by michael on 28-09-15.
+ */
+public class Database {
+    private static Database _instance;
+    private Connection connection = null;
+    private static final String DB_NAME = "";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "";
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String LOCATION = "jdbc:postgresql://localhost:5432/";
+
+    // Private constructor. Prevents instantiation from other classes.
+    private Database() throws SQLException {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        connection = DriverManager.getConnection(LOCATION + DB_NAME,USER,PASSWORD);
+    }
+
+    public static Database getInstance() throws SQLException {
+        if (_instance == null) {
+            _instance = new Database();
+        } else if (_instance.getConnection().isClosed()) {
+            _instance = new Database();
+        }
+
+        return _instance;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+
+}
