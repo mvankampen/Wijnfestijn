0d06c6b1402e353080f515eb5cb25c9fe6744092
Michael <michael1vk@hotmail.com>
2015-11-05 14:30:07 +0100

    Commentaar toegevoegd

--- src/states/SettingsStateInvitational.java
+++ src/states/SettingsStateInvitational.java
@@ -4,99 +4,142 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SettingsStateInvitational implements SettingsState{
-	private String returnBody, returnTitle, defaultBody = "", defaultTitle = "", pathToFile;
-	
-	public SettingsStateInvitational(){
-		fileToVariable();
-	}
-	
-	public void fileToVariable() {
-		String line;
-		//defaultPath from SettingsState class
-		pathToFile = DEFAULTPATH + "INVITATIONAL.html";
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(pathToFile));
-			returnBody = "";
-			returnTitle = "";
-			
-			Pattern pattern = Pattern.compile("<title>(.+?)</title>");
-            Matcher matcher = pattern.matcher(readFile(pathToFile));
-            matcher.find();
+/**
+ * @author Dennis Sloove, Michael van Kampen
+ * @version 0.1, november 2015
+ *          Description:
+ *          SettingsStateInvitational implements the SettingsState, treated the Invitational mail.
+ */
 
-        	returnTitle = matcher.group(1);
-        	defaultTitle = matcher.group(1);
-            
-			while ((line = br.readLine()) != null){
+public class SettingsStateInvitational implements SettingsState {
 
-	        		returnBody += line + "\n";
-					defaultBody += line + "\n";
-		    }
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		private String returnBody, returnTitle, defaultBody = "", defaultTitle = "", pathToFile;
+
+		/**
+		 * <P>Default Constructor</P>
+		 */
+		public SettingsStateInvitational(){
+				fileToVariable();
 		}
-	}
-	
-	public String readFile(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
 
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            return sb.toString();
-        } finally {
-            br.close();
-        }
-    }
-	
-	public void writeToFile(){
-		try {
-			Pattern pattern = Pattern.compile("<title>(.+?)</title>");
-            Matcher matcher = pattern.matcher(readFile(pathToFile));
-            matcher.find();
-            returnBody = returnBody.replace(matcher.group(1), returnTitle);
-			
-			// Create a new PrintWriter
-			PrintWriter pw = new PrintWriter(pathToFile);
-			pw.close();
-			// Re-initialize pw
-			pw = new PrintWriter(new BufferedWriter(new FileWriter(pathToFile, true)));
-			pw.write(returnBody);
-			// Closing the PrintWriter
-			pw.close();
-			defaultBody = returnBody;
-		} catch (IOException e) {
-			e.printStackTrace();
+		/**
+		 * <P>Retrieves the content of the selected file and loads it
+		 * into a variable</P>
+		 */
+		@Override public void fileToVariable() {
+				String line;
+				//defaultPath from SettingsState class
+				pathToFile = DEFAULTPATH + "INVITATIONAL.html";
+				try {
+						BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+						returnBody = "";
+						returnTitle = "";
+
+						Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+						Matcher matcher = pattern.matcher(readFile(pathToFile));
+						matcher.find();
+
+						returnTitle = matcher.group(1);
+						defaultTitle = matcher.group(1);
+
+						while ((line = br.readLine()) != null){
+
+								returnBody += line + "\n";
+								defaultBody += line + "\n";
+						}
+						br.close();
+				} catch (IOException e) {
+						e.printStackTrace();
+				}
 		}
-	}
 
-	public void updateBody(String add){
-		returnBody = add;
-	}
-	
-	public void updateTitle(String add){
-		returnTitle = add;
-	}
+		/**
+		 * @param fileName Location of the file
+		 * @return String with file content
+		 * @throws IOException Signals that an I/O exception of some sort has occurred. This
+		 *                     class is the general class of exceptions produced by failed or
+		 *                     interrupted I/O operations.
+		 */
+		@Override public String readFile(String fileName) throws IOException {
+				BufferedReader br = new BufferedReader(new FileReader(fileName));
+				try {
+						StringBuilder sb = new StringBuilder();
+						String line = br.readLine();
 
-	public String getBody(){
-		return returnBody;
-	}
-	
-	public String getTitle(){
-		return returnTitle;
-	}
-	
-	public String getDefaultBody(){
-		return defaultBody;
-	}
-	
-	public String getDefaultTitle(){
-		return defaultTitle;
-	}
+						while (line != null) {
+								sb.append(line);
+								sb.append("\n");
+								line = br.readLine();
+						}
+						return sb.toString();
+				} finally {
+						br.close();
+				}
+		}
+
+		/**
+		 * @return String with the body of a template
+		 */
+		@Override public String getBody() {
+				return this.returnBody;
+		}
+
+		/**
+		 * @return String with the title of a template
+		 */
+		@Override public String getTitle() {
+				return this.returnTitle;
+		}
+
+		/**
+		 * <P>Write content of the variable to a file</P>
+		 */
+		@Override public void writeToFile() {
+				try {
+						Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+						Matcher matcher = pattern.matcher(readFile(pathToFile));
+						matcher.find();
+						returnBody = returnBody.replace(matcher.group(1), returnTitle);
+
+						// Create a new PrintWriter
+						PrintWriter pw = new PrintWriter(pathToFile);
+						pw.close();
+						// Re-initialize pw
+						pw = new PrintWriter(new BufferedWriter(new FileWriter(pathToFile, true)));
+						pw.write(returnBody);
+						// Closing the PrintWriter
+						pw.close();
+						defaultBody = returnBody;
+				} catch (IOException e) {
+						e.printStackTrace();
+				}
+		}
+
+		/**
+		 * @param add contains new input for the body
+		 */
+		@Override public void updateBody(String add) {
+				this.returnBody = add;
+		}
+
+		/**
+		 * @param add contains new input for the title
+		 */
+		@Override public void updateTitle(String add) {
+				this.returnTitle = add;
+		}
+
+		/**
+		 * @return Default Body
+		 */
+		@Override public String getDefaultBody() {
+				return this.defaultBody;
+		}
+
+		/**
+		 * @return Default Title
+		 */
+		@Override public String getDefaultTitle() {
+				return this.defaultTitle;
+		}
 }
--- src/states/SettingsStateReminder.java
+++ src/states/SettingsStateReminder.java
@@ -4,40 +4,62 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SettingsStateReminder implements SettingsState{
-	private String returnBody, returnTitle, defaultBody = "", defaultTitle = "", pathToFile;
-	
-	public SettingsStateReminder(){
-		fileToVariable();
-	}
-	
-	public void fileToVariable() {
-		String line;
-		//defaultPath from SettingsState class
-		pathToFile = DEFAULTPATH + "REMINDER.html";
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(pathToFile));
-			returnBody = "";
-			returnTitle = "";
-			
-			Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+/**
+ * @author Dennis Sloove, Michael van Kampen
+ * @version 0.1, november 2015
+ *          Description:
+ *          SettingsStateReminder implements the SettingsState, treated the Reminder mail.
+ */
+
+public class SettingsStateReminder implements SettingsState {
+
+    private String returnBody, returnTitle, defaultBody = "", defaultTitle = "", pathToFile;
+
+    /**
+     * <P>Default Constructor</P>
+     */
+    public SettingsStateReminder() {
+        fileToVariable();
+    }
+
+    /**
+     * <P>Retrieves the content of the selected file and loads it
+     * into a variable</P>
+     */
+    @Override public void fileToVariable() {
+        String line;
+        //defaultPath from SettingsState class
+        pathToFile = DEFAULTPATH + "REMINDER.html";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+            returnBody = "";
+            returnTitle = "";
+
+            Pattern pattern = Pattern.compile("<title>(.+?)</title>");
             Matcher matcher = pattern.matcher(readFile(pathToFile));
             matcher.find();
-            
-			while ((line = br.readLine()) != null){
-		        	returnTitle = matcher.group(1);
-		        	defaultTitle = matcher.group(1);
 
-	        		returnBody += line + "\n";
-					defaultBody += line + "\n";
-		    }
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public String readFile(String fileName) throws IOException {
+            while ((line = br.readLine()) != null) {
+                returnTitle = matcher.group(1);
+                defaultTitle = matcher.group(1);
+
+                returnBody += line + "\n";
+                defaultBody += line + "\n";
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param fileName Location of the file
+     * @return String with file content
+     * @throws IOException Signals that an I/O exception of some sort has occurred. This
+     *                     class is the general class of exceptions produced by failed or
+     *                     interrupted I/O operations.
+     */
+    @Override public String readFile(String fileName) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         try {
             StringBuilder sb = new StringBuilder();
@@ -53,49 +75,70 @@ public class SettingsStateReminder implements SettingsState{
             br.close();
         }
     }
-	
-	public void writeToFile(){
-		try {
-			Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+
+    /**
+     * @return String with the body of a template
+     */
+    @Override public String getBody() {
+        return this.returnBody;
+    }
+
+    /**
+     * @return String with the title of a template
+     */
+    @Override public String getTitle() {
+        return this.returnTitle;
+    }
+
+    /**
+     * <P>Write content of the variable to a file</P>
+     */
+    @Override public void writeToFile() {
+        try {
+            Pattern pattern = Pattern.compile("<title>(.+?)</title>");
             Matcher matcher = pattern.matcher(readFile(pathToFile));
             matcher.find();
             returnBody = returnBody.replace(matcher.group(1), returnTitle);
-			
-			// Create a new PrintWriter
-			PrintWriter pw = new PrintWriter(pathToFile);
-			pw.close();
-			// Re-initialize pw
-			pw = new PrintWriter(new BufferedWriter(new FileWriter(pathToFile, true)));
-			pw.write(returnBody);
-			// Closing the PrintWriter
-			pw.close();
-			defaultBody = returnBody;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-	public void updateBody(String add){
-		returnBody = add;
-	}
-	
-	public void updateTitle(String add){
-		returnTitle = add;
-	}
+            // Create a new PrintWriter
+            PrintWriter pw = new PrintWriter(pathToFile);
+            pw.close();
+            // Re-initialize pw
+            pw = new PrintWriter(new BufferedWriter(new FileWriter(pathToFile, true)));
+            pw.write(returnBody);
+            // Closing the PrintWriter
+            pw.close();
+            defaultBody = returnBody;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getBody(){
-		return returnBody;
-	}
-	
-	public String getTitle(){
-		return returnTitle;
-	}
-	
-	public String getDefaultBody(){
-		return defaultBody;
-	}
-	
-	public String getDefaultTitle(){
-		return defaultTitle;
-	}
+    /**
+     * @param add contains new input for the body
+     */
+    @Override public void updateBody(String add) {
+        this.returnBody = add;
+    }
+
+    /**
+     * @param add contains new input for the title
+     */
+    @Override public void updateTitle(String add) {
+        this.returnTitle = add;
+    }
+
+    /**
+     * @return Default Body
+     */
+    @Override public String getDefaultBody() {
+        return this.defaultBody;
+    }
+
+    /**
+     * @return Default Title
+     */
+    @Override public String getDefaultTitle() {
+        return this.defaultTitle;
+    }
 }
