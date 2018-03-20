10d1ac2d71e71cd0e130989c54d8ef6f1e1bd877
Sander de Jong <sanderdejong@outlook.com>
2018-03-20 15:11:43 +0100

    Fix: startup query error

--- src/DAO/Database.java
+++ src/DAO/Database.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class Database {
     private static Database _instance;
     private Connection connection = null;
-    private String DB_NAME = "wijnfestijnDB";
+    private String DB_NAME = "postgres";
     private String USER = "postgres";
     private static final String PASSWORD = "root";
     private static final String DRIVER = "org.postgresql.Driver";
